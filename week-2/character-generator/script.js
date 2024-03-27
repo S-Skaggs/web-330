995dc646bc74fe1315fe4c89d6c3f39d6a53ad40
S-Skaggs <s-skaggs@outlook.com>
2024-03-27 08:29:29 -0400

    FEAT: Working on Assignment2.2 - Closures

--- week-2/character-generator/script.js
+++ week-2/character-generator/script.js
@@ -3,23 +3,85 @@
   Chapter 1
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author: Sheldon Skaggs
+  Date: 3/27/2024
+  Filename: script.js
 */
 
 "use strict";
 
 function createCharacter(name, gender, characterClass) {
-  // TODO: Implement this function
+  // Private properties
+  let charName = name;
+  let charGender = gender;
+  let charClass = characterClass;
+
+  // Return character object
+  return {
+    // Methods to access private properties
+    getName: function() {
+      return charName;
+    },
+    getGender: function() {
+      return charGender;
+    },
+    getClass: function() {
+      return charClass;
+    },
+    // Method for character greeting
+    greeting: function(targetName) {
+      return `Greetings ${targetName}! I am ${charName} the ${charClass}!`;
+    },
+    // Method to get character data
+    getDataAsHTML: function() {
+      return `
+      <p>Name: ${charName}</p>
+      <p>Class: ${charClass}</p>
+      <p>Gender: ${charGender}</p>`;
+    }
+  };
+}
+
+function validateForm() {
+  let isValid = true;
+  let errorMessages = "";
+  // Validate the form
+  if(document.getElementById("heroName").value === "") {
+    errorMessages += "Provide a name your hero<br />";
+    isValid = false;
+  }
+  if(document.getElementById("heroGender").value === "") {
+    errorMessages += "Select a gender for your hero<br />";
+    isValid = false;
+  }
+  if(document.getElementById("heroClass").value === "") {
+    errorMessages += "Select a class for your hero";
+    isValid = false;
+  }
+  if(!isValid) {
+    // Display error message
+    let characterOutput = document.getElementById("characterOutput");
+    characterOutput.className = "invalidForm";
+    characterOutput.innerHTML = "<h2>Missing Input</h2><p>Please correct the following errors:</p>" + errorMessages;
+  }
+
+  return isValid;
 }
 
 document.getElementById("generateHero").addEventListener("click", function(e) {
   e.preventDefault();
+  let characterOutput = document.getElementById("characterOutput");
+  if(validateForm()) {
+    // Get form values
+    let hName = document.getElementById("heroName").value;
+    let hGender = document.getElementById("heroGender").value;
+    let hClass = document.getElementById("heroClass").value;
 
-  // TODO: Get form values
+    // Create character
+    let newCharacter = createCharacter(hName, hGender, hClass);
 
-  // TODO: Create character
-
-  // TODO: Display character information
+    // Display character information
+    characterOutput.className = "characterReadout";
+    characterOutput.innerHTML = `<p>${newCharacter.greeting("User")}</p>${newCharacter.getDataAsHTML()}`;
+  }
 });
