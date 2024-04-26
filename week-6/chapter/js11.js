3cbe85366d6cf4a3f8de9fe77e5107e6c1db6995
S-Skaggs <s-skaggs@outlook.com>
2024-04-26 17:10:03 -0400

    FEAT: Chapter work up to quck check 3. Perl still not working locally.

--- week-6/chapter/js11.js
+++ week-6/chapter/js11.js
@@ -39,7 +39,7 @@ function init() {
    xhr.open("get", "commentary.html");
    xhr.send(null);
 
-   // Retrieve archived articles from teh web server
+   // Retrieve archived articles from the web server
    sButton.onclick = () => {
       fetch("archives.pl?skey=" + encodeURIComponent(sInput.value))
       .then ( response => {
@@ -51,6 +51,63 @@ function init() {
       })
       .then ( comtext => stories.innerHTML = comtext )
       .catch (stories.innerHTML = "Network Failure");
+   }
+
+   // Fetch current headlines from teh web server
+   fetch("headlines.xml")
+   .then (response => response.text())
+   .then (str => new DOMParser().parseFromString(str, "text/xml"))
+
+   // Write the XML content to HTML
+   .then (dom => {
+      let items = dom.querySelectorAll("item");
+      // Loop through each story item
+      for (let story of items) {
+         // Write the story content and append it ot the page
+         let headline = story.children[0].textContent;
+         let link = story.children[1].textContent;
+         let summary = story.children[2].textContent;
+         let htmlCode = `<article><h2><a href="${link}">${headline}</a></h2><p>${summary}</p></article>`;
+         news.insertAdjacentHTML("beforeend", htmlCode);
+      }
+   });
+
+   // Suggest keywords as text is entered in the search box
+   sInput.onkeyup = () => {
+    if(sInput.value === "") {
+      suggestBox.style.display = "none";
+    } else {
+      // Retrieve a list of matching keywords
+      fetch("keywords.pl?suggest=" + encodeURIComponent(sInput.value))
+      .then (response => response.json())
+
+      // Build the suggestion box
+      .then (keywords => {
+        suggestBox.innerHTML = "";
+
+        if (keywords.matches.length === 0) {
+          // No suggestions to display
+          suggestBox.style.display = "none";
+        } else {
+          // Display suggestions
+          suggestBox.style.display = "block";
+
+          // Create a list of suggestions
+          for (let word of keywords.matches) {
+            let suggestion = document.createElement("div");
+            suggestion.textContent = word;
+            suggestBox.appendChild(suggestion);
+
+            // Add suggestion to search box when clicked
+            suggestion.onclick = () => {
+              sInput.value = word;
+              suggestBox.style.display = "none";
+              sButton.click();
+            }
+          }
+        }
+      })
+    }
    }
 
 }
