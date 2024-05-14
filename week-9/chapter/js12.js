41612c4b7ae0b88e419562974763406ac46ceab9
S-Skaggs <s-skaggs@outlook.com>
2024-05-14 06:48:08 -0400

    FEAT: Chapter work in progress. At  page 506.

---
+++ week-9/chapter/js12.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*  JavaScript 7th Edition
+    Chapter 12
+    Chapter case
+
+    Bonsai Expressions FAQ
+    Author: Sheldon Skaggs
+    Date:   5/14/2024
+
+    Filename: js12.js
+*/
+
+// Run once the page is loaded and ready
+$( () => {
+  //
+});
